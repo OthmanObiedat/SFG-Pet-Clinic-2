9292ef1a25af2502655a1dec7943bbb63ce565e9
othman.alothman <othman.alothman@2STP-6892L.secondstep.local>
2020-05-05 10:49:30 +0300

    Add Pet Clinic Index Page and Controller. closes #8

--- pet-clinic-data/src/main/java/org/pet/clinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/model/Owner.java
@@ -2,4 +2,9 @@ package org.pet.clinic.model;
 
 public class Owner extends Person{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 }
--- pet-clinic-web/src/main/java/org/pet/clinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/org/pet/clinic/SfgPetClinicApplication.java
@@ -1,3 +1,5 @@
+package org.pet.clinic;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
