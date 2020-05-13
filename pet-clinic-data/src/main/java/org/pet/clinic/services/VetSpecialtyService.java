367e4243f0fc0aa30f6c62382b5b79f3f0ab90c1
othman.alothman <othman.alothman@2STP-6892L.secondstep.local>
2020-05-13 16:57:47 +0300

    Create Specialty Map Service imp.closes #30

---
+++ pet-clinic-data/src/main/java/org/pet/clinic/services/VetSpecialtyService.java
@@ -0,0 +1,7 @@
+package org.pet.clinic.services;
+
+import org.pet.clinic.model.Speciality;
+
+public interface VetSpecialtyService extends CrudService<Speciality	, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/org/pet/clinic/services/map/VetSpecialityMapService.java
@@ -0,0 +1,42 @@
+package org.pet.clinic.services.map;
+
+import java.util.Set;
+
+import org.pet.clinic.model.Speciality;
+import org.pet.clinic.services.VetSpecialtyService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VetSpecialityMapService extends AbstractMapService<Speciality, Long>implements VetSpecialtyService  {
+
+	@Override
+	public Set<Speciality> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
