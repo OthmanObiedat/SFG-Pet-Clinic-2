94c44aaf6d22da6a909e13ffd5a377171468ddab
othman.alothman <othman.alothman@2STP-6892L.secondstep.local>
2020-05-13 15:32:22 +0300

    add properties. Closes #28

---
+++ pet-clinic-data/src/main/java/org/pet/clinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package org.pet.clinic.model;
+
+public class Speciality extends BaseEntity{
+
+	private String descreption;
+
+	public String getDescreption() {
+		return descreption;
+	}
+
+	public void setDescreption(String descreption) {
+		this.descreption = descreption;
+	}
+}
--- pet-clinic-data/src/main/java/org/pet/clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/model/Vet.java
@@ -14,9 +14,9 @@ public class Vet extends Person{
 	public Set<Speciality> getSpecialties() {
 		return specialties;
 	}
-
 	public void setSpecialties(Set<Speciality> specialties) {
 		this.specialties = specialties;
 	}
+	
 	
 }
