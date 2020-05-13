8cc264422630e7a77d4299f2f0fa463be58710af
othman.alothman <othman.alothman@2STP-6892L.secondstep.local>
2020-05-13 15:18:20 +0300

    add specialty.Closes #27

--- pet-clinic-data/src/main/java/org/pet/clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/model/Vet.java
@@ -1,5 +1,7 @@
 package org.pet.clinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
 	/**
@@ -7,4 +9,14 @@ public class Vet extends Person{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private Set<Speciality>specialties;
+
+	public Set<Speciality> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(Set<Speciality> specialties) {
+		this.specialties = specialties;
+	}
+	
 }
