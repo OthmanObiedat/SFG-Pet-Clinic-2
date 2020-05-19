9cdc2f4dcbf0c4c47abca7cdfc5777d3ccb758e7
othman.alothman <othman.alothman@2STP-6892L.secondstep.local>
2020-05-19 14:11:52 +0300

    Convert Vets to JPA Entities. closes #36

--- pet-clinic-data/src/main/java/org/pet/clinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/model/Speciality.java
@@ -1,7 +1,18 @@
 package org.pet.clinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Column(name = "descreption")
 	private String descreption;
 
 	public String getDescreption() {
--- pet-clinic-data/src/main/java/org/pet/clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/model/Vet.java
@@ -3,15 +3,28 @@ package org.pet.clinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vets_specialities",joinColumns = @JoinColumn(name = "vet_id"),
+	inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality>specialties = new HashSet<Speciality>();
 
+	
 	public Set<Speciality> getSpecialties() {
 		return specialties;
 	}
